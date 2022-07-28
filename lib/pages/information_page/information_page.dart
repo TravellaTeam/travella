bc8e46adadeb0c452f96d89d5cd6c5525c34ca76
Melih Emre Güler <melihemreguler@hotmail.com>
2022-07-28 23:59:13 +0300

    Task-3 information page tasarımını ilerlettim

--- lib/pages/information_page/information_page.dart
+++ lib/pages/information_page/information_page.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:travella_01/pages/gecici_kesfet_sayfasi_widgeti.dart';
 
-class InformationPage extends StatelessWidget {
+var mainColor = const Color.fromARGB(255, 0, 202, 157);
+
+class InformationPage extends StatefulWidget {
   const InformationPage({Key? key}) : super(key: key);
+
+  @override
+  State<InformationPage> createState() => _InformationPageState();
+}
+
+class _InformationPageState extends State<InformationPage> {
+
+  List<Widget> rateValue = [
+    Icon(Icons.circle_rounded, color: mainColor,),
+    Icon(Icons.circle_rounded, color: mainColor,),
+    Icon(Icons.circle_rounded, color: mainColor,),
+    Icon(Icons.circle_rounded, color: mainColor,),
+    Icon(Icons.circle_outlined, color: mainColor,),
+  ];
+
+  List<Widget> actions() {
+    return [
+      IconButton(
+        onPressed: () {
+          print("tapped");
+        },
+        icon: Icon(Icons.star_border),
+      ),
+      IconButton(
+        onPressed: () {
+          print("tapped");
+        },
+        icon: Icon(Icons.alt_route),
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,31 +44,70 @@ class InformationPage extends StatelessWidget {
         body: CustomScrollView(
           slivers: <Widget>[
             SliverAppBar(
+            //toolbarHeight: 45,
+              bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(37),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: double.maxFinite,
+                        padding: const EdgeInsets.only(top: 3, bottom: 2),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 11,),
+                            ...rateValue,
+                            SizedBox(width: 7,),
+                            Text(
+                              "4.2/5.0",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                            SizedBox(width: 7,),
+                            Text(
+                              "   ●   1.3 KM YAKININDA",
+                              style: TextStyle(color: Colors.grey.shade700),
+                            ),
+                          ],
+                        )
+                      ),
+                    ],
+                  )),
               pinned: true,
-              expandedHeight: 250.0,
-              backgroundColor: Colors.lightGreen.shade300,
-              flexibleSpace: const FlexibleSpaceBar(
-
-                title: Text(header),
+              expandedHeight: 300.0,
+              backgroundColor: mainColor,
+              flexibleSpace: FlexibleSpaceBar(
+                titlePadding: const EdgeInsets.only(bottom: 47, left: 55),
+                title: Text(
+                  header,
+                  style: TextStyle(fontSize: 23, color: Colors.grey.shade800),
+                ),
+                background: Image.asset(
+                  "assets/images/saklikent_selalesi_screenshot_2.png",
+                  width: double.maxFinite,
+                  fit: BoxFit.cover,
+                ),
               ),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    print("tapped");
-                    },
-                  icon: Icon(Icons.star_border),
-                ),
-                IconButton(
-                  onPressed: () {
-                    print("tapped");
-                    },
-                  icon: Icon(Icons.alt_route),
-                ),
-              ],
+              leading: IconButton(
+                onPressed: () async {
+                  /*
+                  await Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => AnaSayfaCenterWidget()));
+                   */
+                },
+                icon: Icon(Icons.arrow_back),
+              ),
+              actions: actions(),
             ),
+
             SliverGrid(
               gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200.0,
+                maxCrossAxisExtent: 150.0,
                 mainAxisSpacing: 10.0,
                 crossAxisSpacing: 10.0,
                 childAspectRatio: 4.0,
